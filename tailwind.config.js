4adb31a9a967e4ea2d65381823710f909926d6e5
Ryan Irelan <ryan@mijingo.com>
2021-02-09 17:08:05 -0600

    first commit

---
+++ tailwind.config.js
@@ -0,0 +1,19 @@
+module.exports = {
+  purge: [],
+  darkMode: false, // or 'media' or 'class'
+  theme: {
+    extend: {
+      fontFamily: {
+        logo: ['"Rubik Mono One"', "sans-serif"],
+        tagline: ['"Space Mono"', "sans-serif"],
+        title: ['"PT Sans Narrow"', "sans-serif"],
+        copy: ["Raleway", "sans-serif"]
+
+      }
+    },
+  },
+  variants: {
+    extend: {},
+  },
+  plugins: [],
+}
