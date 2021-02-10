6b464df451bee94c35ef81cd4c064e17517917ed
Ryan Irelan <ryan@mijingo.com>
2021-02-09 22:50:22 -0600

    first final pass at new templates with tailwind

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,6 +3,9 @@ module.exports = {
   darkMode: false, // or 'media' or 'class'
   theme: {
     extend: {
+      backgroundImage: theme => ({
+        'stars-bg': "url('/images/background-stars.svg')"
+      }),
       fontFamily: {
         logo: ['"Rubik Mono One"', "sans-serif"],
         tagline: ['"Space Mono"', "sans-serif"],
